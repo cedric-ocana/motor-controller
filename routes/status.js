f88703c381f94adeb97cd988fa882f369cbfdd16
cedric-ocana <github@ocana.be>
2014-07-14 17:07:38 +0200

    =Added i2c handling for ADC

--- routes/status.js
+++ routes/status.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
+
+function calcAdc(adcValue)
+{
+	var minValue = 0.0303;
+	var vdd = 3.371;
+	var pga = 1;
+	var maxAdcValue = 0x8000;
+	var outputValue = adcValue/(maxAdcValue * pga) * vdd + minValue;
+	outputValue = Math.round(outputValue * 1000)/1000;
+}
+
+var internalGetAdc = function(){
+						return 3.3;
+					 };
+					 
+// Check operating system and load SPI interface if linux is detected.
+if (os.platform() == 'linux')
+{
+	var i2c = require('i2c');
+	var address = 0x48;
+	var wire = new i2c(address , { device: "/dev/i2c-1"});
+	internalGetAdc = function(){
+						wire.writeByte(0x0C, function(err){});
+						res = wire.readBytes(0x0C,3,function(err,res){});
+						return calcAdc(res.readUInt16BE(0));
+					 }
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('status', { title: 'Status' });
+  res.render('status', { title: 'Status', voltage: internalGetAdc()});
 });
 
 module.exports = router;
